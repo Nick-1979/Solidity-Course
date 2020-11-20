8ffee262292a050e2136d48cdaca75ec4e2d8b62
Kami Fard <46442452+Nick-1979@users.noreply.github.com>
2020-11-20 23:31:44 +0330

    Create 2 add coments.sol

---
+++ 2 add coments.sol
@@ -0,0 +1,13 @@
+function getResult() public view returns(uint){
+   // This is a comment. It is similar to comments in C++
+
+   /*
+      * This is a multi-line comment in solidity
+      * It is very similar to comments in C Programming
+   */
+   uint a = 1;
+   uint b = 2;
+   uint result = a + b;
+   
+   return result;
+}
