6e7a16cc7ff5db798fd9fd4bd6c1c6a5c633e7f1
Kami Fard <46442452+Nick-1979@users.noreply.github.com>
2020-11-20 23:29:31 +0330

    First Step
    As a hello wrold

--- 1 simple storage.sol
+++ 1 simple storage.sol
@@ -1,9 +1,11 @@
 pragma solidity >=0.4.0 <0.6.0;
 contract SimpleStorage {
    uint storedData;
+   
    function set(uint x) public {
       storedData = x;
    }
+   
    function get() public view returns (uint) {
       return storedData;
    }
